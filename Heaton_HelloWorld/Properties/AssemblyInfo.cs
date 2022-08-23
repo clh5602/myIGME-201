241f4f8c1360542e9e4987f3e5de9dd80d1ca9c5
clh5602 <clh5602@rit.edu>
2022-08-23 17:33:29 -0400

    Starting Out

--- Heaton_HelloWorld/Properties/AssemblyInfo.cs
+++ Heaton_HelloWorld/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("HelloWorld")]
+[assembly: AssemblyTitle("Heaton_HelloWorld")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("HelloWorld")]
+[assembly: AssemblyProduct("Heaton_HelloWorld")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("283577f8-588d-4608-bef9-8b13522cbf47")]
+[assembly: Guid("095e578e-3ea5-43d9-8e1f-b72c79b55787")]
 
 // Version information for an assembly consists of the following four values:
 //
