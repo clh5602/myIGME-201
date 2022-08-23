1903bd85a85414e32da6e85704307b205a9ef802
clh5602 <clh5602@rit.edu>
2022-08-22 21:59:32 -0400

    Test

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    // Class: Program
+    // Author: Colby Heaton
+    // Purpose: Console Read/Write and Exception-handling exercise
+    //
+    static internal class Program
+    {
+        // Method: Main
+        // Purpose: Prompt the user for their favorite color
+        //          Output
+        // Restrictions: None
+        static void Main(string[] args)
+        {
+
+            // Testing
+        }
+    }
+}
