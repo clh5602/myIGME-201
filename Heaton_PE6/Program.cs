3971d6f2d8c38d689b6fb7e8eb8beb84a50bb750
clh5602 <clh5602@rit.edu>
2022-09-24 22:28:25 -0400

    Division Added

--- Heaton_PE6/Program.cs
+++ Heaton_PE6/Program.cs
@@ -17,62 +17,34 @@ namespace Heaton_PE6
         //          Give the user 8 attempts to guess the number, giving feedback
         //          after every attempt on if the real value is higher or lower than the guess.
         // Restrictions: None
+        struct order
+        {
+            public string itemName;
+            public int unitCount;
+            public double unitCost;
+
+
+            public double orderPrice()
+            {
+                return unitCount * unitCost;
+            }
+
+            public string orderReceipt()
+            {
+                return String.Format("{0} {1} items at ${2} each, total cost ${3}", unitCount, itemName, unitCost, orderPrice());
+            }
+
+        }
+
         static void Main(string[] args)
         {
-            const int MAX_ATTEMPTS = 8;
-            const int UPPER_BOUNDS = 100;
-            const int LOWER_BOUNDS = 0;
-            int numAttempts = 0;
-            int userGuess = 0;
+            order ex = new order();
+            ex.itemName = "Joe";
+            ex.unitCount = 4;
+            ex.unitCost = 1.25;
+            
+            Console.WriteLine(ex.orderReceipt());  
 
-            // Generate Random Number
-            Random rand = new Random();
-
-            // generate a random number between 0 inclusive and 101 exclusive
-            int randomNumber = rand.Next(LOWER_BOUNDS, UPPER_BOUNDS + 1);
-
-            // Print the random number
-            Console.WriteLine(randomNumber);
-            Console.WriteLine("Try to guess the random number between 0-100 - you have 8 tries!");
-
-            // Begin a for loop for # of attempts
-            for (numAttempts = 1; numAttempts <= MAX_ATTEMPTS; ++numAttempts)
-            {
-                // Prompt the user for an input
-                Console.Write("Turn #" + numAttempts + ": Enter your guess: ");
-
-                // While loop to parse submission & check range
-                while (!(int.TryParse(Console.ReadLine(), out userGuess) && userGuess <= UPPER_BOUNDS && userGuess >= LOWER_BOUNDS))
-                {
-                    Console.WriteLine("Invalid guess – try again");
-                    Console.Write("Turn #" + numAttempts + ": Enter your guess: ");
-                }
-                // If equal, break loop
-                if (userGuess == randomNumber)
-                {
-                    break;
-                }
-
-                // Compare submission to random value
-                if (userGuess > randomNumber)
-                {
-                    Console.WriteLine("Too high");
-                } else
-                {
-                    Console.WriteLine("Too low");
-                }
-            }
-
-
-            // Print results based on # of attempts
-            if (numAttempts <= MAX_ATTEMPTS)
-            {
-                Console.WriteLine("Correct! You won in " + numAttempts + " turn(s).");
-            }
-            else
-            {
-                Console.WriteLine("You ran out of turns. The number was " + randomNumber + ".");
-            }
-        }
+    }
     }
 }
