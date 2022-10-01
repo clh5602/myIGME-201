06c407a9889626aa98483505626e97fc91917e43
clh5602 <clh5602@rit.edu>
2022-09-30 22:17:17 -0400

    complete

---
+++ Vehicles/Class1.cs
@@ -0,0 +1,30 @@
+﻿namespace Vehicles
+{
+    public abstract class Vehicle
+    {
+        public virtual void LoadPassenger() { }
+    }
+
+    public interface IPassengerCarrier
+    {
+        public void LoadPassenger() { }
+    }
+
+    public interface IHeavyLoadCarrier { }
+
+    public abstract class Car : Vehicle { }
+
+    public abstract class Train : Vehicle { }
+
+    public class Compact: Car, IPassengerCarrier { }
+    public class SUV : Car, IPassengerCarrier { }
+    public class Pickup : Car, IPassengerCarrier, IHeavyLoadCarrier { }
+    public class PassengerTrain : Train, IPassengerCarrier { }
+    public class FreightTrain : Train, IHeavyLoadCarrier { }
+    public class _424DoubleBogey : Train, IHeavyLoadCarrier { }
+
+
+
+
+
+}
