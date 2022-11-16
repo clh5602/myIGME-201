c8fdc9121b12d6ba1d73fda18f36d016000ce62a
clh5602 <clh5602@rit.edu>
2022-11-16 10:47:37 -0500

    initial commit

---
+++ Heaton_PE17/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Heaton_PE17
+{
+    partial class startForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.lowLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.startButton = new System.Windows.Forms.Button();
+            this.lowTextBox = new System.Windows.Forms.TextBox();
+            this.highTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Location = new System.Drawing.Point(63, 26);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(159, 13);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "Enter Range of Values to Guess";
+            // 
+            // lowLabel
+            // 
+            this.lowLabel.AutoSize = true;
+            this.lowLabel.Location = new System.Drawing.Point(40, 74);
+            this.lowLabel.Name = "lowLabel";
+            this.lowLabel.Size = new System.Drawing.Size(70, 13);
+            this.lowLabel.TabIndex = 1;
+            this.lowLabel.Text = "Low Number:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 119);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "High Number:";
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(101, 162);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 3;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            // 
+            // lowTextBox
+            // 
+            this.lowTextBox.Location = new System.Drawing.Point(145, 71);
+            this.lowTextBox.Name = "lowTextBox";
+            this.lowTextBox.Size = new System.Drawing.Size(100, 20);
+            this.lowTextBox.TabIndex = 4;
+            this.lowTextBox.Text = "1";
+            // 
+            // highTextBox
+            // 
+            this.highTextBox.Location = new System.Drawing.Point(145, 116);
+            this.highTextBox.Name = "highTextBox";
+            this.highTextBox.Size = new System.Drawing.Size(100, 20);
+            this.highTextBox.TabIndex = 5;
+            this.highTextBox.Text = "100";
+            // 
+            // startForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 210);
+            this.Controls.Add(this.highTextBox);
+            this.Controls.Add(this.lowTextBox);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lowLabel);
+            this.Controls.Add(this.titleLabel);
+            this.Name = "startForm";
+            this.Text = "Guessing Game";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label lowLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.TextBox lowTextBox;
+        private System.Windows.Forms.TextBox highTextBox;
+    }
+}
+
---
+++ Heaton_PE17/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Heaton_PE17
+{
+    public partial class startForm : Form
+    {
+        public startForm()
+        {
+            InitializeComponent();
+            this.startButton.Click += new EventHandler(this.StartButton_Click);
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            bool bConv;
+            int lowNumber = 0;
+            int highNumber = 0;
+
+            // convert the strings entered in lowTextBox and highTextBox
+            // to lowNumber and highNumber Int32.Parse
+
+            if (!Int32.TryParse(lowTextBox.Text, out lowNumber)) { MessageBox.Show("The numbers are invalid."); return; }
+            if (!Int32.TryParse(highTextBox.Text, out highNumber)) { MessageBox.Show("The numbers are invalid."); return; }
+
+            // if not a valid range
+            if (lowNumber > highNumber)
+            {
+                // show a dialog that the numbers are not valid
+                MessageBox.Show("The numbers are invalid.");
+            }
+            else
+            {
+                // otherwise we're good
+                // create a form object of the second form 
+                // passing in the number range
+                GameForm gameForm = new GameForm(lowNumber, highNumber);
+
+                // display the form as a modal dialog, 
+                // which makes the first form inactive
+                gameForm.ShowDialog();
+            }
+        }
+
+    }
+}
