c480f324f033dbd0a89538b6a965056f6f791126
clh5602 <clh5602@rit.edu>
2022-11-24 01:23:44 -0500

    complete

---
+++ Heaton_JsonHtml/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using PeopleAppGlobals;
+using PeopleLib;
+using System.IO;
+
+namespace Heaton_JsonHtml
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Globals.AddPeopleSampleData();
+
+            List<Teacher> teachers = new List<Teacher>();
+            List<Student> students = new List<Student>();
+
+            foreach(KeyValuePair<string, Person> keyValuePair in Globals.people.sortedList)
+            {
+                if(keyValuePair.Value.GetType() == typeof(Teacher))
+                {
+                    teachers.Add((Teacher)keyValuePair.Value);
+                }
+                else
+                {
+                    students.Add((Student)keyValuePair.Value);
+                }
+            }
+
+            string s = JsonConvert.SerializeObject(students);
+            string t = JsonConvert.SerializeObject(teachers);
+
+            StreamWriter writer = new StreamWriter("c:/temp/students.json");
+            writer.Write(s);
+            writer.Close();
+
+            writer = new StreamWriter("c:/temp/teachers.json");
+            writer.Write(t);
+            writer.Close();
+
+            StreamReader reader = new StreamReader("c:/temp/students.json");
+            s = reader.ReadToEnd();
+            reader.Close();
+
+            reader = new StreamReader("c:/temp/teachers.json");
+            t = reader.ReadToEnd();
+            reader.Close();
+
+            students = JsonConvert.DeserializeObject<List<Student>>(s);
+
+            teachers = JsonConvert.DeserializeObject<List<Teacher>>(t);
+
+            SortedList<string, Person> people = new SortedList<string, Person>();
+
+            foreach(Student student in students)
+            {
+                people[student.email] = student;
+            }
+
+            foreach (Teacher teacher in teachers)
+            {
+                people[teacher.email] = teacher;
+            }
+
+        }
+    }
+}
