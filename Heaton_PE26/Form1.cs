f16be1746e412c39a51afc6c872d9e10d0841ccf
clh5602 <clh5602@rit.edu>
2022-11-24 00:54:39 -0500

    complete

---
+++ Heaton_PE26/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Heaton_PE26
+{
+    public partial class SettingsForm : Form
+    {
+        public int myPort;
+
+        public SettingsForm(Form owner, int nPort)
+        {
+            InitializeComponent();
+            this.Owner = owner;
+            this.CenterToParent();
+            this.myPort = nPort;
+            this.portTextBox.Text = nPort.ToString();
+            this.startButton.Click += new EventHandler(StartButton__Click);
+            this.portTextBox.KeyPress += new KeyPressEventHandler(PortTextBox__KeyPress);
+        }
+
+        private void StartButton__Click(object sender, EventArgs e)
+        {
+            this.myPort = Int32.Parse(this.portTextBox.Text);
+            this.Close();
+        }
+
+        private void PortTextBox__KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (char.IsDigit(e.KeyChar) || e.KeyChar == '\b')
+            {
+                e.Handled = false;
+            }
+            else
+            {
+                e.Handled = true;
+            }
+            
+        }
+    }
+}
