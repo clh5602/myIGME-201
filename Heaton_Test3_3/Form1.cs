4fe732f26b71041fe75c850f72f5e2814526a23e
clh5602 <clh5602@rit.edu>
2022-11-30 23:44:51 -0500

    2 forms

--- Heaton_Test3_3/Form1.cs
+++ Heaton_Test3_3/Form1.cs
@@ -18,17 +18,75 @@ namespace Heaton_Test3_3
 
             searchButton.Click += new EventHandler(SearchButton__Click);
             exitButton.Click += new EventHandler(ExitButton__Click);
+
+            radioButton1.Click += new EventHandler(RadioButton__Click);
+            radioButton2.Click += new EventHandler(RadioButton__Click);
+            radioButton3.Click += new EventHandler(RadioButton__Click);
+
+            searchBox.KeyPress += new KeyPressEventHandler(TextBox__KeyPress);
+
+            fyootleLabel.Click += new EventHandler(Fyootle__Click);
         }
 
         private void SearchButton__Click(object sender, EventArgs e)
         {
-            Form myForm = new browserForm(searchBox.Text);
+            string engine = "";
+
+            if (radioButton1.Checked)
+            {
+                engine = "https://www.google.com/search?q=";
+            }
+            if (radioButton2.Checked)
+            {
+                engine = "https://www.bing.com/search?q=";
+            }
+            if (radioButton3.Checked)
+            {
+                engine = "http://www.youtube.com/results?search_query=";
+            }
+            Form myForm = new browserForm(engine + searchBox.Text);
+            myForm.Show();
+        }
+
+        private void Fyootle__Click(object sender, EventArgs e)
+        {
+            Form myForm = new browserForm("https://www.google.com/search?q=futile");
             myForm.Show();
         }
 
         private void ExitButton__Click(object sender, EventArgs e)
         {
             Application.Exit();
+        }
+
+        private void RadioButton__Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            int goodButton = rnd.Next(0, 3);
+
+            switch (goodButton)
+            {
+                case 0:
+                    radioButton1.Checked = true;
+                    break;
+                case 1:
+                    radioButton2.Checked = true;
+                    break;
+                case 2:
+                    radioButton3.Checked = true;
+                    break;
+            }
+        }
+
+        private void TextBox__KeyPress(object sender, KeyPressEventArgs e)
+        {
+            Random rnd = new Random();
+            int failNum = rnd.Next(0, 5);
+
+            if (failNum == 0)
+            {
+                e.Handled = true;
+            }
         }
     }
 }
--- Heaton_Test3_3/Form2.cs
+++ Heaton_Test3_3/Form2.cs
@@ -15,14 +15,37 @@ namespace Heaton_Test3_3
         public browserForm()
         {
             InitializeComponent();
+
         }
 
         public browserForm(string query)
         {
             InitializeComponent();
 
+            webBrowser.DocumentCompleted += new WebBrowserDocumentCompletedEventHandler(WebBrowser__DocumentCompleted);
+
             webBrowser.Url = new Uri(query);
             this.Text = query;
+        }
+
+        private void WebBrowser__DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            WebBrowser webBrowser = (WebBrowser)sender;
+            this.Text = webBrowser.Url.ToString();
+            HtmlElementCollection htmlElementCollection = webBrowser.Document.Body.GetElementsByTagName("*");
+
+            Random rnd = new Random();
+
+            foreach (HtmlElement htmlElement in htmlElementCollection)
+            {
+                int failNum = rnd.Next(0, 15);
+
+                if (failNum == 0)
+                {
+                    htmlElement.InnerHtml = "";
+                }
+                
+            }
         }
     }
 }
