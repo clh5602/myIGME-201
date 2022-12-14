9274b5b8118376bc5bc3f277a860cc00ae52326e
clh5602 <clh5602@rit.edu>
2022-12-14 11:51:43 -0500

    complete

---
+++ Heaton_Final4/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heaton_Final4
+{
+    enum EColor
+    {
+        red,
+        blue,
+        yellow,
+        cyan,
+        gray,
+        purple,
+        orange,
+        green
+    }
+
+
+    internal class Program
+    {
+        static int[,] colorMGraph = new[,]
+        {               /*r  b   y   c   g   p  o   ge*/
+            /*red*/     {-1, 1, -1, -1, 5, -1, -1, -1},
+            /*blue*/    {-1, -1, 8, 1, -1, -1, -1, -1},
+            /*yellow*/  {-1, -1, -1, -1, -1, -1, -1, 6},
+            /*cyan*/    {-1, 1, -1, -1, 0, -1, -1, -1},
+            /*gray*/    {-1, -1, -1, 0, -1, -1, 1, -1},
+            /*purple*/  {-1, -1, 1, -1, -1, -1, -1, -1},
+            /*orange*/  {-1, -1, -1, -1, -1, 1, -1, -1},
+            /*green*/   {-1, -1, -1, -1, -1, -1, -1, -1}
+        };
+
+        static int[][] colorAGraph = new int[][]
+        {
+            /*red*/     new int[] {(int)EColor.blue, (int)EColor.gray},
+            /*blue*/    new int[] {(int)EColor.yellow, (int)EColor.cyan},
+            /*yellow*/  new int[] {(int)EColor.green},
+            /*cyan*/    new int[] {(int)EColor.blue, (int)EColor.gray},
+            /*gray*/    new int[] {(int)EColor.cyan, (int)EColor.orange},
+            /*purple*/  new int[] {(int)EColor.yellow},
+            /*orange*/  new int[] {(int)EColor.purple},
+            /*green*/   null
+        };
+
+        static int[][] colorWGraph = new int[][]
+        {
+            /*red*/     new int[] {1, 5},
+            /*blue*/    new int[] {8, 1},
+            /*yellow*/  new int[] {6},
+            /*cyan*/    new int[] {1, 0},
+            /*gray*/    new int[] {0, 1},
+            /*purple*/  new int[] {1},
+            /*orange*/  new int[] {1},
+            /*green*/   null
+        };
+
+        static void DFS(EColor eState)
+        {
+            bool[] visited = new bool[colorAGraph.Length];
+
+            DFSUtil(eState, visited);
+        }
+
+        static void DFSUtil(EColor v, bool[] visited)
+        {
+            visited[(int)v] = true ;
+
+            Console.Write(v.ToString() + " ");
+
+            int[] thisStateList = colorAGraph[(int)v];
+            if (thisStateList != null)
+            {
+                foreach (int n in thisStateList)
+                {
+                    if (!visited[n])
+                    {
+                        DFSUtil((EColor)n, visited);
+                    }
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            // DFS Search
+            DFS(EColor.red);
+        }
+    }
+}
